8d4050465d9bef57de7f809e142d9b3c06db0147
Peter Tränkle <peter.traenkle@liip.ch>
2026-05-25 18:17:02 +0200

    added custom DHCP lease viewer script to network-tools.nix

--- modules/common/network-tools.nix
+++ modules/common/network-tools.nix
@@ -27,6 +27,24 @@
       
       # Analysis (CLI versions)
       wireshark-cli # tshark, etc.
+
+      # Custom DHCP lease viewer
+      (pkgs.writeShellScriptBin "show-leases" ''
+        printf "%-19s %-17s %-15s %-20s %s\n" "Expires" "MAC Address" "IP Address" "Hostname" "Client ID"
+        printf "%s\n" "--------------------------------------------------------------------------------------------------"
+        if [ ! -f /var/lib/dnsmasq/dnsmasq.leases ]; then
+          echo "No leases file found at /var/lib/dnsmasq/dnsmasq.leases"
+          exit 0
+        fi
+        while read -r expires mac ip host cid; do
+          if [ "$expires" -eq 0 ]; then
+            date_str="Static/Infinite"
+          else
+            date_str=$(date -d "@$expires" "+%Y-%m-%d %H:%M:%S")
+          fi
+          printf "%-19s %-17s %-15s %-20s %s\n" "$date_str" "$mac" "$ip" "$host" "$cid"
+        done < /var/lib/dnsmasq/dnsmasq.leases
+      '')
     ];
   };
 }
